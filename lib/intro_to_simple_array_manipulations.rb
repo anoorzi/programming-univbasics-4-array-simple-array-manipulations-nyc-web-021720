41a0d8f4e6f4c7720d2cc718e4d9b5b5b0463a07
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-13 09:53:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array,el)
+  array.push(el)
+end
+
+def using_unshift(array,el)
+  array.unshift(el)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,num = 2)
+  array.pop(2)
+
+end
